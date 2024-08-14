008e1cb11ca066943e40a6cdc89c075f2d113ab4
Edu <luis_100404@hotmail.com>
2024-08-14 01:09:39 -0300

    add files for coding

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Projeto facul</title>
+</head>
+</html>
+
+<?php include "./partials/navbar.php"; ?>
+<body>
+    <h1>Olá</h1>
+</body>
+<?php include "./partials/footer.php"?>
---
+++ pages/sobre.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Projeto facul</title>
+</head>
+</html>
+
+<?php include "../partials/navbar.php"; ?>
+
+
+<h2>depois decido</h2>
